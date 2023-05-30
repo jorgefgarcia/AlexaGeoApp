54d90f8b35a6fe5d7fb64aa504d12d8b4c06f44b
jorgefgarcia <jorge.fgarcia@udc.es>
2023-05-30 11:19:30 +0200

    Añadidos nuevos intentsHandlers y cambios en las frases

--- AlexaSkill/index.js
+++ AlexaSkill/index.js
@@ -102,6 +102,67 @@ const GetCuriosityHandler = {
     }
 };
 
+const CancelAndStopHandler = {
+    canHandle(handlerInput){
+        return handlerInput.requestEnvelope.request.type == 'IntentRequest'
+        && ( handlerInput.requestEnvelope.request.intent.name === 'AMAZON.NoIntent'
+            || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'  
+            || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
+    },
+    async handle(handlerInput){
+        return operations.getData()
+        .then((data) => {
+            if(`${data}` == "true"){
+                const speechText = "De acuerdo, nos vemos!"
+                return handlerInput.responseBuilder
+                .speak(speechText)
+                .getResponse();
+            }else{
+                var speechText = `Error en la autenticación, actualmente no está autenticado. No voy a resolver su petición`
+                return handlerInput.responseBuilder
+                .speak(speechText)
+                .getResponse();
+            }
+        })
+        .catch((err)=>{
+            const speechText = "Hubo un error al coger el dato";
+            return handlerInput.responseBuilder
+            .speak(speechText)
+            .getResponse();
+        })
+    }
+};
+
+const HelpIntentHandler = {
+    canHandle(handlerInput){
+        return handlerInput.requestEnvelope.request.type == 'IntentRequest'
+        && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+    },
+    async handle(handlerInput){
+        return operations.getData()
+        .then((data) => {
+            if(`${data}` == "true"){
+                const speechText = "Cuando la autenticación sea satisfactoria puedes pedirme una curiosidad. Para comprobar el estado de la autenticación diga: estoy autenticado."
+                return handlerInput.responseBuilder
+                .speak(speechText)
+                .reprompt(speechText)
+                .getResponse(); 
+            }else{
+                var speechText = `Lo siento, no puedo ayudarle, no está autenticado correctamente`
+                return handlerInput.responseBuilder
+                .speak(speechText)
+                .getResponse();
+            }
+        })
+        .catch((err)=>{
+            const speechText = "Hubo un error al coger el dato";
+            return handlerInput.responseBuilder
+            .speak(speechText)
+            .getResponse();
+        })
+    }
+};
+
 const ErrorHandler = {
     canHandle() {
         return true;
@@ -122,14 +183,14 @@ function getRandomItem(array){
 }
 
 const CURIOSIDADES = [
-'El 95% de los ataques cibernéticos se deben a errores humanos.',
-'Un ataque de phising ocurre cada 30 segundos.',
-'Los hackers tardan en promedio 191 días en detectar una violación de seguridad.',
-'El ransomware es el tipo de ataque cibernético más costoso, con un costo promedio de recuperación de 1.85 millones de dólares.',
-'El robo de los datos personales es una de las principales preocupaciones de los usuarios en la era digital.',
-'Las contraseñas débiles son la principal causa de violaciones de seguridad.',
-'La ingeniería social es una técnica comúnmente utilizada por los hackers para engañar a las personas y obtener información confidencial.',
-'La inteligencia artificial y el aprendizaje automático son cada vez más utilizados para mejorar la seguridad en línea.'
+'El 95% de los ataques cibernéticos se deben a errores humanos. ',
+'Un ataque de phising ocurre cada 30 segundos. ',
+'Los hackers tardan en promedio 191 días en detectar una violación de seguridad. ',
+'El ransomware es el tipo de ataque cibernético más costoso, con un costo promedio de recuperación de 1.85 millones de dólares. ',
+'El robo de los datos personales es una de las principales preocupaciones de los usuarios en la era digital. ',
+'Las contraseñas débiles son la principal causa de violaciones de seguridad. ',
+'La ingeniería social es una técnica comúnmente utilizada por los hackers para engañar a las personas y obtener información confidencial. ',
+'La inteligencia artificial y el aprendizaje automático son cada vez más utilizados para mejorar la seguridad en línea. '
 ];
 
 const PREGUNTAS = [
@@ -149,7 +210,9 @@ exports.handler = Alexa.SkillBuilders.custom()
         LaunchRequestHandler,
         GetWelcomeHandler,
         GetDataHandler,
-        GetCuriosityHandler)
+        GetCuriosityHandler,
+        CancelAndStopHandler,
+        HelpIntentHandler)
     .addErrorHandlers(
         ErrorHandler)
     .withApiClient(new Alexa.DefaultApiClient())
